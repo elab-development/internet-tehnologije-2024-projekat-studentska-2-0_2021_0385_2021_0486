e4ecc093796655a8f448990852177c1e4a689dd0
Nikola <95249108+NSoldatovic@users.noreply.github.com>
2025-10-06 16:18:09 +0200

    Enhance Course and ExamEnrollment models: add fillable properties and relationships; refactor Student model to extend Authenticatable and include additional properties

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'naziv',
+        'sifra_predmeta',
+        'espb',
+    ];
+
+    public function examEnrollments()
+    {
+        return $this->hasMany(ExamEnrollment::class);
+    }
+
+    public function students()
+    {
+        return $this->belongsToMany(Student::class, 'exam_enrollments');
+    }
 }
